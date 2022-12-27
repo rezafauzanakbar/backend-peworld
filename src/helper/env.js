4fe87e63fac4fb8856ee47e894efd2ae06713828
Reza Fauzan Akbar <rezafauzanakbar3@gmail.com>
2022-12-27 19:37:26 +0700

    update config

--- src/helper/env.js
+++ src/helper/env.js
@@ -5,6 +5,5 @@ module.exports = {
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
   DB_PORT: process.env.DB_PORT,
-  PORT: process.env.PORT,
   JWT_SECRET: process.env.JWT_SECRET,
 };
